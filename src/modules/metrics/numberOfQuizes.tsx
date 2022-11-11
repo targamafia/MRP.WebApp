4ad9ea872d2a87616428e1d5e68f843e08bcc8ae
Dpalme <diegoPalmerin24@gmail.com>
2022-11-10 20:19:29 -0600

    generalized metric component

--- src/modules/metrics/numberOfQuizes.tsx
+++ src/modules/metrics/numberOfQuizes.tsx
@@ -1,25 +1,16 @@
-import { ErrorMessage } from '@/shared/components/errorMessage';
-import { LoadingSpinner } from '@/shared/components/loadingSpinner';
-import { NavLink } from 'react-router-dom';
 import { useAssessments } from '../assessments/hooks/useAssessments';
+import MetricCard from './components/metricCard';
 
 const NumberOfQuizesMetric = () => {
   const { assessments, error, loading } = useAssessments();
   return (
-    <NavLink
+    <MetricCard
       to="assessments"
-      className="p-8 grow bg-surface-4 text-center border border-orange rounded-md
-    hover:scale-105 transition-all hover:bg-surface-5"
-    >
-      {loading ? (
-        <LoadingSpinner />
-      ) : error ? (
-        <ErrorMessage message={error.toString()} />
-      ) : (
-        <h2 className="text-3xl">{assessments.length}</h2>
-      )}
-      <p>Quizes totales</p>
-    </NavLink>
+      loading={loading}
+      error={error}
+      data={assessments && assessments.length}
+      label="Quizes totales"
+    />
   );
 };
 
